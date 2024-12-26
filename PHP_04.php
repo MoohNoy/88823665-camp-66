75c7796eff9301948996e1077c6a70f51d0c6c89
66160348 <Moohchokchai.3000@gmail.com>
2024-12-26 11:41:11 +0700

    Update#3

--- PHP_04.php
+++ PHP_04.php
@@ -1,19 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
+<html lang="en">
+<head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </head>
-    <body>
-        <div class="container mt-5">
+<body>
+<div class="container mt-5">
             <form method="POST" action="">
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Email address</label>
                     <input name="email" type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label"> Enter the multiplication table. </label>
-                    <input name="detail" type="number" class="form-control" id="exampleFormControlInput2" placeholder="1 2 3">
+                    <label for="exampleFormControlTextarea1" class="form-label"> Enter a number </label>
+                    <input name="detail" type="number" class="form-control" id="exampleFormControlInput2" >
                 </div>
                 <div class="mb-3">
                     <button class="btn btn-success" type="submit">Submit</button>
@@ -24,15 +23,15 @@
             </form>
             <?php 
             if ($_SERVER["REQUEST_METHOD"] == "POST") {?>
-            <div class="h2 col text-start mt-2">
+            <div class="h2 col text-start mt-2  ">
                 <?php  if($_POST["email"]!=null && $_POST["detail"]!=null){
                     echo $_POST["email"];
-                    $mul = $_POST["detail"]; 
-                    for ($i = 1; $i <= 12; $i++) {
+                    $num = $_POST["detail"]; 
+                    for ($i = 1; $i <= $num; $i++) {
                         ?>
-                        <div class="row">
-                            <div class="h2 col text-start"> 
-                                <?php echo $mul; ?> x <?php echo $i; ?> = <?php echo ($mul * $i); ?> 
+                        <div class="row ">
+                            <div class="h2 col text-start mt-1"> 
+                            <?php echo $i; ?> เป็นเลข <?php if($i%2==0){echo "คู่" ; }else{echo "คี่" ;}  ?> 
                             </div>
                         </div>
                     <?php 
@@ -41,6 +40,6 @@
                 } 
                 ?>
             </div>
-        </div>
-    </body>
+</div>
+</body>
 </html>
