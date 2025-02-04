29f88f36d882459634a725c3f1550d0225e7bf03
66160348 <Moohchokchai.3000@gmail.com>
2025-02-04 15:15:34 +0700

    Update#1 edit delete

--- mylaravel/app/Http/Controllers/Registercontroller.php
+++ mylaravel/app/Http/Controllers/Registercontroller.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class Registercontroller extends Controller
 {
     function index(){
         return view("register");
+    }
+    function create(Request $req){
+        $User = User::create([
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => $req->password,
+        ]);
+        return redirect('/user');
     }
 }
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Mycontroller;
 use App\Http\Controllers\Logincontroller;
 use App\Http\Controllers\Registercontroller;
 use App\Http\Controllers\Homecontroller;
+use App\Http\Controllers\Usercontroller;
 
 Route::get('/hello', function () {
     return "<h1>Happy very much!</h1>";
@@ -30,3 +31,22 @@ Route::get("/login",
 [Logincontroller::class,'index']);
 Route::post("/login", 
 [Logincontroller::class,'index']);
+
+Route::get('/register',  
+[Registercontroller::class,'index']);
+Route::post('/register',  
+[Registercontroller::class,'create']);
+
+
+
+Route::get('/user/{id}',  
+[Usercontroller::class,'edit']);
+
+Route::get('/user',  
+[Usercontroller::class,'index']);
+
+Route::put('/user' ,
+[Usercontroller::class, 'edit_user']);
+
+Route::delete('/user',
+[Usercontroller::class, 'delete']);
